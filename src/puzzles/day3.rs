06dc536f400a39fe1b79817f729d7cd4b103f4bb
austindingtian <austindingtian@users.noreply.github.com>
2025-12-11 05:53:37 +0100

    Solve Day 3 Part Two

--- src/puzzles/day3.rs
+++ src/puzzles/day3.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 use crate::utils::load_puzzle_input;
 
 /*
@@ -31,35 +33,76 @@ pub fn solution(input_path: &str) {
             banks.push(line);
         }
     }
+
+    // part 1
     let mut total_output_joltage: usize = 0;
-    for bank in banks {
-        total_output_joltage+= find_largest_possible_joltage(&bank);
+    for bank in &banks {
+        total_output_joltage+= find_output_joltage(&bank, 2);
     }
     println!("Day 3 Part One answer: {total_output_joltage}");
+
+    // part 2 
+    for bank in &banks {
+        total_output_joltage+= find_output_joltage(&bank, 12);
+    }
+    println!("Day 3 Part Two answer: {total_output_joltage}");
 }
 
-fn find_largest_possible_joltage(bank: &str) -> usize {
+/* 
+Solution for part2
+---------------------
+Previously in part1 I did the same solution but hard coded for 2 batteries in the output joltage.
+Here I just abstracted that to arbitrary number of batteries `n`.
+So this works for part1 as well.
+Idea is you have to have at least `n` batteries from the end,
+so you can make sure you fill up the requisite `n` batteries in the output joltage.
+Worst case scenario O(n^2) so uhh not great but alas.
+ */
+fn find_output_joltage(bank: &str, joltage_size: usize) -> usize {
+    let bank_size = bank.len();
     let mut current_largest_joltage = "0";
-    let mut clj_index = 0;
-    for i in 0..bank.len()-1 {
-        let joltage = &bank[i..i+1];
+    let mut next_start_index = 0;
+    let mut output_joltage = String::new();
 
-        if joltage > current_largest_joltage {
-            current_largest_joltage = joltage;
-            clj_index = i;
-        }
+    for digits_remaining in (0..joltage_size).rev() {
+        for i in next_start_index..bank_size-digits_remaining {
+            let joltage = &bank[i..i+1];
+    
+            if joltage > current_largest_joltage {
+                current_largest_joltage = joltage;
+                next_start_index = i + 1;
+            }
+        }  
+       //println!("Found largest joltage {current_largest_joltage} at index {}. Starting next iteration in this bank at index {next_start_index}", next_start_index - 1);
+        output_joltage.push_str(current_largest_joltage);
+        current_largest_joltage = "0";
     }
+    //println!("{output_joltage}");
+    output_joltage.parse::<usize>().unwrap()
 
-    let mut second_largest_joltage = "0";
-    for i in clj_index+1..bank.len() {
-        let joltage = &bank[i..i+1];
-
-        if joltage > second_largest_joltage {
-            second_largest_joltage = joltage;
-        }
-    }
-
-    let maximum_joltage = format!("{current_largest_joltage}{second_largest_joltage}").parse::<usize>().unwrap();
-    //println!("{maximum_joltage}");
-    maximum_joltage
 }
+// fn find_largest_possible_joltage(bank: &str) -> usize {
+//     let mut current_largest_joltage = "0";
+//     let mut clj_index = 0;
+//     for i in 0..bank.len()-1 {
+//         let joltage = &bank[i..i+1];
+
+//         if joltage > current_largest_joltage {
+//             current_largest_joltage = joltage;
+//             clj_index = i;
+//         }
+//     }
+
+//     let mut second_largest_joltage = "0";
+//     for i in clj_index+1..bank.len() {
+//         let joltage = &bank[i..i+1];
+
+//         if joltage > second_largest_joltage {
+//             second_largest_joltage = joltage;
+//         }
+//     }
+
+//     let maximum_joltage = format!("{current_largest_joltage}{second_largest_joltage}").parse::<usize>().unwrap();
+//     //println!("{maximum_joltage}");
+//     maximum_joltage
+// }
